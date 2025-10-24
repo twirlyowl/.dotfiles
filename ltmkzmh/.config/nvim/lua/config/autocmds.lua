69a04dd8bd107719e00d8c40850230b7b37f53c5
Twirly Owl <twirlyowl@protonmail.com>
2025-10-24 13:07:47 -0400

    changing some settings in nvim

--- ltmkzmh/.config/nvim/lua/config/autocmds.lua
+++ ltmkzmh/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Show raw Markdown (don’t conceal backticks/fences)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.wo.conceallevel = 0
+    vim.wo.concealcursor = ""
+  end,
+})
+
+-- Stop continuing comments/lists on new lines
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    -- remove "c", "r", "o" just in case any are present
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
